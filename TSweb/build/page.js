dc5f3976d3b8aa0cc63cb4ae08d958120cb45293
quanjiaxin <1411514716@qq.com>
2020-12-27 21:41:25 +0800

    ts函数重载

--- TSweb/build/page.js
+++ TSweb/build/page.js
@@ -1,2 +1,40 @@
 "use strict";
-console.log(123456);
+var Header = /** @class */ (function () {
+    function Header() {
+    }
+    Header.prototype.constractor = function () {
+        var elem = document.createElement('div');
+        console.log(elem);
+        elem.innerHTML = 'This is header';
+        document.body.appendChild(elem);
+    };
+    return Header;
+}());
+var Content = /** @class */ (function () {
+    function Content() {
+    }
+    Content.prototype.constractor = function () {
+        var elem = document.createElement('div');
+        elem.innerHTML = 'This is header';
+        document.body.appendChild(elem);
+    };
+    return Content;
+}());
+var Footer = /** @class */ (function () {
+    function Footer() {
+    }
+    Footer.prototype.constractor = function () {
+        var elem = document.createElement('div');
+        elem.innerHTML = 'This is header';
+        document.body.appendChild(elem);
+    };
+    return Footer;
+}());
+var Page = /** @class */ (function () {
+    function Page() {
+        new Header();
+        new Content();
+        new Footer();
+    }
+    return Page;
+}());
--- TSweb/src/page.ts
+++ TSweb/src/page.ts
@@ -1,1 +1,32 @@
-console.log(123456)
+class Header {
+  constractor() {
+    const elem = document.createElement('div')
+    console.log(elem)
+    elem.innerHTML = 'This is header'
+    document.body.appendChild(elem)
+  }
+}
+
+class Content {
+  constractor() {
+    const elem = document.createElement('div')
+    elem.innerHTML = 'This is header'
+    document.body.appendChild(elem)
+  }
+}
+
+class Footer {
+  constractor() {
+    const elem = document.createElement('div')
+    elem.innerHTML = 'This is header'
+    document.body.appendChild(elem)
+  }
+}
+
+class Page {
+  constructor() {
+    new Header()
+    new Content()
+    new Footer()
+  }
+}
